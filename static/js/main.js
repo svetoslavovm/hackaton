4eea33c4d687c4bbe832b7b3ec5e56cdd8fb3dfb
svetoslavovm <svetoslavov.martin@gmail.com>
2016-01-30 18:21:34 -0500

    actives footer donate button

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,9 @@
+'use strict';
 
+document.addEventListener('DOMContentLoaded', function(event) {
+  var donateButton = document.getElementsByClassName('go-to-donate')[0];
+
+  donateButton.addEventListener('click', function() {
+    document.location.href = '/donate.html';
+  });
+});
